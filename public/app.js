17f07239619b454a602bb790249e1c8bd3a32070
TareqNewazShahriar <tareq.newaz.shahriar@gmail.com>
2020-05-28 22:24:01 +0600

    turn url

--- public/app.js
+++ public/app.js
@@ -7,9 +7,9 @@ const configuration = {
 		// 	]
 		// }
 		{
-			urls: "turn:asia.myturnserver.net",
-			username: "allie@oopcode.com",
-			credential: "topsecretpassword"
+			url: 'turn:192.158.29.39:3478?transport=udp',
+			credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+			username: '28224511:1379330808'
 		}
 	],
 	iceCandidatePoolSize: 10,
