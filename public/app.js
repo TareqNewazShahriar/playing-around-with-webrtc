61bc925a01d8a8b13a13e89805a1140fa61f25ac
TareqNewazShahriar <tareq.newaz.shahriar@gmail.com>
2020-05-28 22:42:39 +0600

    turn

--- public/app.js
+++ public/app.js
@@ -7,9 +7,9 @@ const configuration = {
 		// 	]
 		// }
 		{
-			url: 'turn:192.158.29.39:3478?transport=tcp',
-			credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-			username: '28224511:1379330808'
+			url: 'turn:turn.bistri.com:80',
+			credential: 'homeo',
+			username: 'homeo'
 		}
 	],
 	iceCandidatePoolSize: 10,
