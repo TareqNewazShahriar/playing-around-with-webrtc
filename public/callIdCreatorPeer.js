734b2aadf18e0e56589e588202b14e16622f8889
tareq.newaz.shahriar <tareq.newaz.shahriar@gmail.com>
2021-05-13 05:53:37 +0600

    space indentation

--- public/callIdCreatorPeer.js
+++ public/callIdCreatorPeer.js
@@ -2,116 +2,122 @@ import Constants from './Constants.js';
 import WebRtcHelper from './webRtcHelper.js';
 
 export default class CallIdCreatorPeer {
-	helper = null;
+   helper = null;
 
-	peerConnection = null;
-	localStream = null;
-	remoteStream = null;
-	remoteStreamList = [];
+   peerConnection = null;
+   localStream = null;
+   remoteStream = null;
+   remoteStreamList = [];
 
-	dcPeerConnection = null;
-	dataChannel = null;
-	dataChannelOpened = false;
+   dcPeerConnection = null;
+   dataChannel = null;
+   dataChannelOpened = false;
 
-	constructor() {
-		this.helper = new WebRtcHelper();
-	}
+   constructor() {
+      this.helper = new WebRtcHelper();
+   }
 
-	async createCallId(e) {
-		document.querySelector('#createBtn').disabled = true;
-		document.querySelector('#joinBtn').disabled = true;
-		document.querySelector('#hangupBtn').disabled = false;
+   async createCallId(e) {
+      document.querySelector('#createBtn').disabled = true;
+      document.querySelector('#joinBtn').disabled = true;
+      document.querySelector('#hangupBtn').disabled = false;
 
-		let callRef = await this.helper.getDbEntityReference("calls");
-		this.localStream = await this.helper.openUserMedia(e);
-		this.peerConnection = this.helper.initializePeerConnection();
-		this.helper.addTracksToLocalStream(this.peerConnection, this.localStream);
-		this.helper.gatherLocalIceCandidates(this.peerConnection, callRef, 'callerCandidates');
+      let callRef = await this.helper.getDbEntityReference("calls");
+      this.localStream = await this.helper.openUserMedia(e);
+      this.peerConnection = this.helper.initializePeerConnection();
+      this.helper.addTracksToLocalStream(this.peerConnection, this.localStream);
+      this.helper.gatherLocalIceCandidates(this.peerConnection, callRef, 'callerCandidates');
 
-		await this.createOffer(this.peerConnection, callRef);
-		document.getElementById('createdCallId').value = callRef.id;
-		
-		this.remoteStream = this.helper.initRemoteStream(this.peerConnection);
-		await this.listeningForAnswerSdp(this.peerConnection, callRef);
-		await this.helper.gatherRemoteIceCandidates(this.peerConnection, callRef, 'calleeCandidates');
-		this.ringWhenConnected(this.peerConnection, this.remoteStream);
+      await this.createOffer(this.peerConnection, callRef);
+      document.getElementById('createdCallId').value = callRef.id;
 
-		document.querySelector('#hangupBtn').addEventListener('click', e => this.helper.hangUp(e, this.peerConnection, this.remoteStream, "calls", callRef.id));
-	}
+      this.remoteStream = this.helper.initRemoteStream(this.peerConnection);
+      await this.listeningForAnswerSdp(this.peerConnection, callRef);
+      await this.helper.gatherRemoteIceCandidates(this.peerConnection, callRef, 'calleeCandidates');
+      this.ringWhenConnected(this.peerConnection, this.remoteStream);
 
-	async createOffer(peerConnection, entityRef) {
-		const offer = await peerConnection.createOffer();
-		await peerConnection.setLocalDescription(offer);
-		log('Created offer:', offer);
+      document.querySelector('#hangupBtn').addEventListener('click', e => this.helper.hangUp(e, this.peerConnection, this.remoteStream, "calls", callRef.id));
+   }
 
-		const callWithOffer = {
-			'offer': {
-				type: offer.type,
-				sdp: offer.sdp,
-			},
-		};
-		await entityRef.set(callWithOffer);
-		log(`New entity created with SDP offer. entity ID: ${entityRef.id}`);
-	}
+   async createOffer(peerConnection, entityRef) {
+      const offer = await peerConnection.createOffer();
+      await peerConnection.setLocalDescription(offer);
+      log('Created offer:', offer);
 
-	async listeningForAnswerSdp(peerConnection, entityRef) {
-		entityRef.onSnapshot(async snapshot => {
-			const data = snapshot.data();
-			if (!peerConnection.currentRemoteDescription && data && data.answer) {
-				log('Got remote description: ', data.answer);
-				const rtcSessionDescription = new RTCSessionDescription(data.answer);
-				await peerConnection.setRemoteDescription(rtcSessionDescription);
-			}
-		});
-	}
+      const callWithOffer = {
+         'offer': {
+            type: offer.type,
+            sdp: offer.sdp,
+         },
+      };
+      await entityRef.set(callWithOffer);
+      log(`New entity created with SDP offer. entity ID: ${entityRef.id}`);
+   }
 
-	ringWhenConnected(peerConnection, remoteStream) {
-		let ringtone = document.getElementById('ringtone');
-		peerConnection.addEventListener('connectionstatechange', e => {
-			// if rigntone available, play it
-			if (ringtone && peerConnection.connectionState === 'connected') {
-				ringtone.play();
-				let ans = document.getElementById('ans');
-				ans.onclick = function () {
-					ringtone.pause();
-					ringtone.currentTime = 0;
-					document.querySelector('#remoteVideo').srcObject = remoteStream;
-					ans.style.display = 'none';
-				}
-				ans.style.display = 'block';
-			}
-		});
-	}
+   async listeningForAnswerSdp(peerConnection, entityRef) {
+      entityRef.onSnapshot(async snapshot => {
+         const data = snapshot.data();
+         if (!peerConnection.currentRemoteDescription && data && data.answer) {
+            log('Got remote description: ', data.answer);
+            const rtcSessionDescription = new RTCSessionDescription(data.answer);
+            await peerConnection.setRemoteDescription(rtcSessionDescription);
+         }
+      });
+   }
 
-	async initDataChannel() {
-		let dcRef = await this.helper.getDbEntityReference("dataChannels");
-		this.dcPeerConnection = this.helper.initializePeerConnection();
-		this.dataChannel = this.dcPeerConnection.createDataChannel("anyName");
-		this.helper.gatherLocalIceCandidates(this.dcPeerConnection, dcRef, "callerCandidates");
+   ringWhenConnected(peerConnection, remoteStream) {
+      let ringtone = document.getElementById('ringtone');
+      peerConnection.addEventListener('connectionstatechange', e => {
+         // if rigntone available, play it
+         if (ringtone && peerConnection.connectionState === 'connected') {
+            ringtone.play();
+            let ans = document.getElementById('ans');
+            ans.onclick = function () {
+               ringtone.pause();
+               ringtone.currentTime = 0;
+               document.querySelector('#remoteVideo').srcObject = remoteStream;
+               ans.style.display = 'none';
+            }
+            ans.style.display = 'block';
+         }
+      });
+   }
 
-		this.createOffer(this.dcPeerConnection, dcRef);
-		document.getElementById('dcId').value = dcRef.id;
-		document.getElementById('dc-pannel').style.display = 'block';
+   async initDataChannel() {
+      let dcRef = await this.helper.getDbEntityReference("dataChannels");
+      this.dcPeerConnection = this.helper.initializePeerConnection();
+      this.dataChannel = this.dcPeerConnection.createDataChannel("anyName");
+      this.helper.gatherLocalIceCandidates(this.dcPeerConnection, dcRef, "callerCandidates");
 
-		this.listeningForAnswerSdp(this.dcPeerConnection, dcRef);
-		await this.helper.gatherRemoteIceCandidates(this.dcPeerConnection, dcRef, "calleeCandidates");
-		// no data-clear for now
+      this.createOffer(this.dcPeerConnection, dcRef);
+      document.getElementById('dcId').value = dcRef.id;
+      document.getElementById('dc-pannel').style.display = 'block';
+
+      this.listeningForAnswerSdp(this.dcPeerConnection, dcRef);
+      await this.helper.gatherRemoteIceCandidates(this.dcPeerConnection, dcRef, "calleeCandidates");
+      // no data-clear for now
 
 
-		this.dataChannel.addEventListener('open', event => {
-			this.dataChannelOpened = true;
-			log('data channel opened.', event);
+      this.dataChannel.addEventListener('open', event => {
+         this.dataChannelOpened = true;
+         log('data channel opened.', event);
 
-			this.dataChannel.send(JSON.stringify({ type: Constants.DataChannelTransferType.message, data: { name: 'ok', msg: 'hey' } }));
-		})
-		this.dataChannel.addEventListener('close', event => {
+         this.dataChannel.send(JSON.stringify({
+            type: Constants.DataChannelTransferType.message,
+            data: {
+               name: 'ok',
+               msg: 'hey'
+            }
+         }));
+      })
+      this.dataChannel.addEventListener('close', event => {
          this.dataChannelOpened = false;
-			log('data channel closed.', event);
-		});
-		this.dataChannel.addEventListener('message', event => {
-			log('data received:', event.data.data);
-		});
-	}
+         log('data channel closed.', event);
+      });
+      this.dataChannel.addEventListener('message', event => {
+         log('data received:', event.data.data);
+      });
+   }
 
    sendFile(file) {
       log(`File is ${[file.name, file.size, file.type, file.lastModified].join(' ')}`);
@@ -119,18 +125,21 @@ export default class CallIdCreatorPeer {
       this.dataChannel.send(JSON.stringify({
          type: Constants.DataChannelTransferType.message,
          comingType: Constants.DataChannelTransferType.binaryData,
-         data: { size: file.size, name: file.name }
+         data: {
+            size: file.size,
+            name: file.name
+         }
       }));
 
       let fileTransferProgress = document.querySelector('#fileTransferProgress');
       fileTransferProgress.max = file.size;
       fileTransferProgress.value = 0;
-   
+
       let currentOffset = 0;
       let fileReader = new FileReader();
       fileReader.addEventListener('error', error => log('Error reading file:', error));
       fileReader.addEventListener('abort', event => log('File reading aborted:', event));
-      
+
       fileReader.addEventListener('load', e => {
          this.dataChannel.send(e.target.result);
          currentOffset += e.target.result.byteLength;
--- public/callIdUserPeer.js
+++ public/callIdUserPeer.js
@@ -4,15 +4,15 @@ import WebRtcHelper from './webRtcHelper.js';
 'use strict';
 
 export default class CallIdUserPeer {
-	helper = null;
+   helper = null;
 
-	peerConnection = null;
-	localStream = null;
-	remoteStream = null;
+   peerConnection = null;
+   localStream = null;
+   remoteStream = null;
 
-	dcPeerConnection = null;
-	dataChannel = null;
-	dataChannelOpened = false;
+   dcPeerConnection = null;
+   dataChannel = null;
+   dataChannelOpened = false;
 
    fileSize = null;
    fileName = null;
@@ -22,94 +22,94 @@ export default class CallIdUserPeer {
    fileTransferBegin = null;
 
 
-	constructor() {
-		this.helper = new WebRtcHelper();
-	}
+   constructor() {
+      this.helper = new WebRtcHelper();
+   }
 
-	async joinCall(e) {
-		document.querySelector('#createBtn').disabled = true;
-		document.querySelector('#joinBtn').disabled = true;
-		document.querySelector('#hangupBtn').disabled = false;
-		
-		await this.joinCallById(e, prompt("Enter Call ID"));
-	}
+   async joinCall(e) {
+      document.querySelector('#createBtn').disabled = true;
+      document.querySelector('#joinBtn').disabled = true;
+      document.querySelector('#hangupBtn').disabled = false;
 
-	async joinCallById(e, callId) {
-		let callRef = await this.helper.getDbEntityReference("calls", callId);
-		const callSnapshot = await callRef.get();
-		log('Got call record:', callSnapshot.exists);
-		if (!callSnapshot.exists) {
-			alert('Call ID not found');
-			return;
-		}
-		
-		this.localStream = await this.helper.openUserMedia(e);
-		this.peerConnection = this.helper.initializePeerConnection();
-		this.helper.addTracksToLocalStream(this.peerConnection, this.localStream);
-		this.helper.gatherLocalIceCandidates(this.peerConnection, callRef, 'calleeCandidates');
-		this.remoteStream  = this.helper.initRemoteStream(this.peerConnection);
-		await this.createAnswer(this.peerConnection, callRef);
-		await this.helper.gatherRemoteIceCandidates(this.peerConnection, callRef, 'callerCandidates');
+      await this.joinCallById(e, prompt("Enter Call ID"));
+   }
 
-		document.querySelector('#remoteVideo').srcObject = this.remoteStream;
-		document.querySelector('#hangupBtn').addEventListener('click', e => this.helper.hangUp(e, this.peerConnection, this.remoteStream, "calls", callId));
-	}
+   async joinCallById(e, callId) {
+      let callRef = await this.helper.getDbEntityReference("calls", callId);
+      const callSnapshot = await callRef.get();
+      log('Got call record:', callSnapshot.exists);
+      if (!callSnapshot.exists) {
+         alert('Call ID not found');
+         return;
+      }
 
-	async createAnswer(peerConnection, entityRef) {
-		const snapshot = await entityRef.get();
-		const offer = snapshot.data().offer;
-		log('Got offer:', offer);
-		await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-		const answer = await peerConnection.createAnswer();
-		log('Created answer:', answer);
-		await peerConnection.setLocalDescription(answer);
+      this.localStream = await this.helper.openUserMedia(e);
+      this.peerConnection = this.helper.initializePeerConnection();
+      this.helper.addTracksToLocalStream(this.peerConnection, this.localStream);
+      this.helper.gatherLocalIceCandidates(this.peerConnection, callRef, 'calleeCandidates');
+      this.remoteStream = this.helper.initRemoteStream(this.peerConnection);
+      await this.createAnswer(this.peerConnection, callRef);
+      await this.helper.gatherRemoteIceCandidates(this.peerConnection, callRef, 'callerCandidates');
 
-		const withAnswer = {
-			answer: {
-				type: answer.type,
-				sdp: answer.sdp,
-			},
-		};
-		await entityRef.update(withAnswer);
-	}
+      document.querySelector('#remoteVideo').srcObject = this.remoteStream;
+      document.querySelector('#hangupBtn').addEventListener('click', e => this.helper.hangUp(e, this.peerConnection, this.remoteStream, "calls", callId));
+   }
 
-	async initDataChannel() {
-		let dcId = prompt("Data Channel ID");
-		let dcRef = await this.helper.getDbEntityReference("dataChannels", dcId);
-		if(!(await dcRef.get()).exists) {
-			alert(`Data Channel record not found: ${dcId}`);
-			log(`Data Channel record not found: ${dcId}`);
-			return;
-		}
+   async createAnswer(peerConnection, entityRef) {
+      const snapshot = await entityRef.get();
+      const offer = snapshot.data().offer;
+      log('Got offer:', offer);
+      await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+      const answer = await peerConnection.createAnswer();
+      log('Created answer:', answer);
+      await peerConnection.setLocalDescription(answer);
 
-		this.dcPeerConnection = this.helper.initializePeerConnection();
-		await this.helper.gatherLocalIceCandidates(this.dcPeerConnection, dcRef, "calleeCandidates");
-		await this.createAnswer(this.dcPeerConnection, dcRef);
-		await this.helper.gatherRemoteIceCandidates(this.dcPeerConnection, dcRef, "callerCandidates");
+      const withAnswer = {
+         answer: {
+            type: answer.type,
+            sdp: answer.sdp,
+         },
+      };
+      await entityRef.update(withAnswer);
+   }
 
-		this.dcPeerConnection.addEventListener('datachannel', event => {
-			this.dataChannel = event.channel;
-			log('data channel received.', event);
+   async initDataChannel() {
+      let dcId = prompt("Data Channel ID");
+      let dcRef = await this.helper.getDbEntityReference("dataChannels", dcId);
+      if (!(await dcRef.get()).exists) {
+         alert(`Data Channel record not found: ${dcId}`);
+         log(`Data Channel record not found: ${dcId}`);
+         return;
+      }
 
-			this.dataChannel.addEventListener('open', event => {
-				this.dataChannelOpened = true;
-				log('data channel opened.', event);
-			})
-			this.dataChannel.addEventListener('close', event => {
+      this.dcPeerConnection = this.helper.initializePeerConnection();
+      await this.helper.gatherLocalIceCandidates(this.dcPeerConnection, dcRef, "calleeCandidates");
+      await this.createAnswer(this.dcPeerConnection, dcRef);
+      await this.helper.gatherRemoteIceCandidates(this.dcPeerConnection, dcRef, "callerCandidates");
+
+      this.dcPeerConnection.addEventListener('datachannel', event => {
+         this.dataChannel = event.channel;
+         log('data channel received.', event);
+
+         this.dataChannel.addEventListener('open', event => {
+            this.dataChannelOpened = true;
+            log('data channel opened.', event);
+         })
+         this.dataChannel.addEventListener('close', event => {
             this.dataChannelOpened = false;
-				log('data channel closed.', event);
-			});
-			this.dataChannel.addEventListener('message', event => this.onMessageReceived(event));
-		});
-	}
+            log('data channel closed.', event);
+         });
+         this.dataChannel.addEventListener('message', event => this.onMessageReceived(event));
+      });
+   }
 
    onMessageReceived(event) {
       log('data received', event.data);
 
-      if(typeof event.data === 'string') { // prepare to receive binary data on next message event
+      if (typeof event.data === 'string') { // prepare to receive binary data on next message event
          let data = JSON.parse(event.data);
 
-         if(data.comingType === Constants.DataChannelTransferType.binaryData) {
+         if (data.comingType === Constants.DataChannelTransferType.binaryData) {
             this.dataChannel.binaryType = Constants.DataChannelTransferType.binaryData;
             this.fileSize = data.data.size;
             this.fileName = data.data.name;
@@ -119,8 +119,7 @@ export default class CallIdUserPeer {
             this.receivedSize = 0;
             this.fileTransferBegin = (new Date()).getTime();
          }
-      }
-      else if(event.data instanceof ArrayBuffer) { // Now binary data is coming
+      } else if (event.data instanceof ArrayBuffer) { // Now binary data is coming
          let data = event.data;
          this.receiveBuffer.push(data);
          this.receivedSize += data.byteLength;
